cb8da0887bd9b918dbed710942fe259b79fbe434
harufuma <cy25131@shibaura-it.ac.jp>
2025-12-08 20:55:32 +0900

    ボタン作成

--- src/app/components/sellForm/sellForm.jsx
+++ src/app/components/sellForm/sellForm.jsx
@@ -1,10 +1,14 @@
 "use client"
 import  { useState } from 'react';
+import "./sell-form.css";
 
 export default function SellForm(){
     const [subjectName, setSubjectName] = useState("");
     const [textbookName, setTextbookName] = useState("");
     const [desiredPrice, setDesiredPrice] = useState("");
+
+    const [condition, setCondition] = useState("新品未使用"); 
+    const [deliveryMethod, setDeliveryMethod] = useState("大宮キャンパス"); 
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -12,11 +16,12 @@ export default function SellForm(){
         console.log('科目名:', subjectName);
         console.log('教科書名:', textbookName);
         console.log('希望価格:', desiredPrice + '円');
-        
+        console.log('状態:', condition);
+        console.log('受け渡し方法:', deliveryMethod);
     }
 
 return(
-    <form onSubmit={handleSubmit}>
+    <form className="sell-form" onSubmit={handleSubmit}>
         <label>
             科目名:
             <input
@@ -46,6 +51,104 @@ return(
                 required
             />
         </label>
+
+
+
+        <label>
+            <input 
+                type="radio" 
+                name="condition" 
+                value="新品未使用" 
+                checked={condition === "新品未使用"} 
+                onChange={(e) => setCondition(e.target.value)} 
+            />
+            新品未使用
+        </label>
+
+        <label>
+            <input 
+                type="radio" 
+                name="condition" 
+                value="やや傷・汚れあり" 
+                checked={condition === "やや傷・汚れあり"} 
+                onChange={(e) => setCondition(e.target.value)} 
+            />
+            やや傷・汚れあり
+        </label>
+
+      <label>
+        <input 
+          type="radio" 
+          name="condition" 
+          value="未使用に近い" 
+          checked={condition === "未使用に近い"} 
+          onChange={(e) => setCondition(e.target.value)} 
+        />
+        未使用に近い
+      </label>
+
+      <label>
+        <input 
+          type="radio" 
+          name="condition" 
+          value="傷・汚れあり" 
+          checked={condition === "傷・汚れあり"} 
+          onChange={(e) => setCondition(e.target.value)} 
+        />
+        傷・汚れあり
+      </label>
+
+      <label>
+        <input 
+          type="radio" 
+          name="condition" 
+          value="目立った傷・汚れなし" 
+          checked={condition === "目立った傷・汚れなし"} 
+          onChange={(e) => setCondition(e.target.value)} 
+        />
+        目立った傷・汚れなし
+      </label>
+      <label>
+        <input 
+          type="radio" 
+          name="condition" 
+          value="書き込みあり" 
+          checked={condition === "書き込みあり"} 
+          onChange={(e) => setCondition(e.target.value)} 
+        />
+        書き込みあり
+      </label>
+
+      <label>
+      <input 
+        type="radio" 
+        name="delivery" 
+        value="豊洲キャンパス" 
+        checked={deliveryMethod === "豊洲キャンパス"} 
+        onChange={(e) => setDeliveryMethod(e.target.value)} 
+      />
+      豊洲キャンパス
+    </label>
+    <label>
+      <input 
+        type="radio" 
+        name="delivery" 
+        value="大宮キャンパス" 
+        checked={deliveryMethod === "大宮キャンパス"} 
+        onChange={(e) => setDeliveryMethod(e.target.value)} 
+      />
+      大宮キャンパス
+    </label>
+    <label>
+      <input 
+        type="radio" 
+        name="delivery" 
+        value="郵送" 
+        checked={deliveryMethod === "郵送"} 
+        onChange={(e) => setDeliveryMethod(e.target.value)} 
+      />
+      郵送
+    </label>
 
         <button type="submit">出品</button>
     </form>
--- src/app/purchase/page.jsx
+++ src/app/purchase/page.jsx
@@ -1,28 +1,14 @@
 "use client";
-import CheckKeyword from "../_components/CheckKeyWord/Checkkeyword";
-import Header from "../_components/Header/Header";
-import SerectCategory from "../_components/SerectCategory/SerectCategory";
-import UserLog from "../_components/UserLog/UserLog";
-import styles from "./page.module.css";
-export default function HomeMain(){
+import Header from "../components/Header/Header";
+
+
+
+export default function PurchasePage(){
     const PageTitle ="購入する"
-    const ImgSrc="/cart.png"
+    const ImgSrc=""
     return (
         <>
-        <header>
-            <Header className="Header" pageTitle={PageTitle} imgSrc={ImgSrc}/>
-        </header>
-        <main>
-            <UserLog/>
-            <div className={styles.CheckKeyword}>
-                <CheckKeyword/>
-            </div>
-            <div className={styles.SerectCategorys}>
-            <SerectCategory categoryWord={"学部・学科から探す"}/>
-                <SerectCategory categoryWord={"学年・学期で探す"}/>
-                <SerectCategory categoryWord={"科目でさがす"}/>
-            </div>
-        </main>
+        <Header pageTitle={PageTitle}/>
         </>
     );
 }
