f0d72db8f5fd7b2709dff07c2dba0834bc03a863
ItoTsuzuya <tuzuya1220@gmail.com>
2025-12-09 12:14:28 +0900

    12/9 12:14時点での状態

--- src/app/_components/SignUpForm/SignUpForm.jsx
+++ src/app/_components/SignUpForm/SignUpForm.jsx
@@ -15,18 +15,17 @@ export default function SignUpForm(){
         confirmPassword:"",
     });
     const [errors, setErrors] = useState({
-        initial:"",
-        nickname:"",
-        email:"",
-        password:"",
-        confirmPassword:"",
+        initial:null,
+        nickname:null,
+        email:null,
+        password:null,
+        confirmPassword:null,
     });
+    //touchedは、ユーザーが触った項目にのみエラー表示を発火させるためのboolean値
     const [touched, setTouched] = useState({
         email:false,
         password:false,
         confirmPassword:false,
-        initial:false,
-        nickname:false
     });
 
     const router = useRouter();
@@ -37,25 +36,33 @@ export default function SignUpForm(){
 
 
     //Todo: mock用関数を利用して、データサーバとの通信時の非同期処理の仮型の作成
-    const mockSignUp = async() => {
+    const mockSignUp = newPromise((resolve) => {
         setTimeout(() => {
-            console.log("mock用待機時間2秒が終了しました。");
-        }, 2000);
-    } 
+            console.log("supabase通信時間として2秒待機");
+            resolve();
+        }, 2000)
+    })
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        // if(password !== confirmPassword){
-        //     setError("パスワードが一致しません");
-        //     return;
-        // }
+
+        //会員登録録ボタンが押されたときに、handleSubmitですべての項目を一括バリデーション
+        const emailError = validateEmail(values.email);
+        const passwordError = validatePassword(values.password);
+        const confirmPasswordError = validateConfirmPassword(values.password, values.confirmPassword);
+        if(emailError && passwordError && confirmPasswordError === null){
+            console.log("バリエーション成功、purchaseページへ遷移");
+            //Todo:　Supabase認証後に画面が遷移するようにする
+            router.push("/purchase");
+        }else{
+            console.log("バリエーション失敗、エラー表示")
+        }
+
         //ここでぜe.target.valueとしないのか？
         //A. onSubmitの時は、e.targetは<form>タグを指すので、valueがないから。
         //e.target.valueはonChangeの時に使う
         //Todo: Supabase連携後にデータ送信の処理を追加する
-        console.log("入力されたデータ:", { initial, email, nickname, password, confirmPassword });
-        //Todo:　Supabase認証後に画面が遷移するようにする
-        router.push("/purchase");
+        console.log("入力されたデータ:", values);
     }
 
     const handleChange = (fieldName, newValue) => {
@@ -103,10 +110,16 @@ export default function SignUpForm(){
                     //入力されるたびにstate更新＋errorのクリア
                     //→入力が終わったタイミングでバリデーション実行して、最終的なerrorをstateにセットする
                     onChange={(e) => handleChange("email", e.target.value)}
-                    onBlur={(e) => setErrors((prev) => ({
-                        ...prev,
-                        email: validateEmail(e.target.value)
-                    }))}
+                    onBlur={(e) => {
+                        setErrors((prev) => ({
+                            ...prev,
+                            email: validateEmail(e.target.value)
+                        }));
+                        setTouched((prev) => ({
+                            ...prev,
+                            email: true
+                        }));
+                    }}
                     required
                 />
             </label>
@@ -117,7 +130,16 @@ export default function SignUpForm(){
                     type="password"
                     value={values.password}
                     onChange={(e) => handleChange("password", e.target.value)}
-                    onBlur={(e) => validatePassword(e.target.value)}
+                    onBlur={(e) => {
+                        setErrors((prev) => ({
+                            ...prev,
+                            password: validatePassword(e.target.value)
+                        }));
+                        setTouched((prev) => ({
+                            ...prev,
+                            password: true
+                        }));
+                    }}
                     required
                 />
             </label>
@@ -128,7 +150,16 @@ export default function SignUpForm(){
                     type="password"
                     value={values.confirmPassword}
                     onChange={(e) => handleChange("confirmPassword", e.target.value)}
-                    onBlur={(e) => validateConfirmPassword(values.password, e.target.value)}
+                    onBlur={(e) => {
+                        setErrors((prev) => ({
+                            ...prev,
+                            confirmPassword: validateConfirmPassword(values.password, e.target.value)
+                        }));
+                        setTouched((prev) => ({
+                            ...prev,
+                            confirmPassword: true
+                        }));
+                    }}
                     required
                 />
             </label>
