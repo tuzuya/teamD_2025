b99c5b19445eee7479107a35f71daef49d47a21b
ItoTsuzuya <tuzuya1220@gmail.com>
2026-01-23 19:59:52 +0900

    1/23　更新

---
+++ src/app/components/SellPage/SellPage.jsx
@@ -0,0 +1,12 @@
+use "client";
+import { useState } from "react";
+
+export default function SellPage(){
+    const [ textName, setTextName] = useState("");
+    const [ imageUrl, setImageUrl] = useState("");
+    const [ price, setPrice] = useState("");
+    const [ textSubject, setTextSubject ] = useState("");
+    const [ textState, setTextState ] = useState("");
+    const [ deliveryMethod, setDeliveryMethod ] = useState("");
+    
+}
