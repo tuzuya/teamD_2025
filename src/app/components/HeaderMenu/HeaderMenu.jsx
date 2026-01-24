2f999a06d81e5cdd0030e1d97583a9820dd13767
ItoTsuzuya <tuzuya1220@gmail.com>
2026-01-25 06:29:14 +0900

    1/25途中経過

--- src/app/components/HeaderMenu/HeaderMenu.jsx
+++ src/app/components/HeaderMenu/HeaderMenu.jsx
@@ -8,10 +8,10 @@ export default function HeaderMenu({open}){
     <nav className={`${styles.HambargerMenu} ${open ? styles.active : ""}`}>
         <ul className={styles.MenuList}>
             <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/cart.png'} word={'購入する'}/></li>
-            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/cart.png'} word={'購入する'}/></li>
-            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/cart.png'} word={'購入する'}/></li>
-            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/cart.png'} word={'購入する'}/></li>
-            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/cart.png'} word={'購入する'}/></li>
+            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/sell/picturebutton.png'} word={'出品する'}/></li>
+            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/like/heart_white.png'} word={'いいね'}/></li>
+            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/mypage/person.png'} word={'マイページ'}/></li>
+            <li className={styles.MenuItem}><HeaderMenuBtn iconSorce={'/search_result/icon_boy.png'} word={'ログイン・会員登録'}/></li>
         </ul>
     </nav>
     </>
--- src/app/components/HeaderMenu/HeaderMenuBtn.jsx
+++ src/app/components/HeaderMenu/HeaderMenuBtn.jsx
@@ -1,7 +1,11 @@
 "use client";
 import styles from "./HeaderMenuBtn.module.css"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function HeaderMenuBtn({iconSorce,word}){
+    const router = useRouter();
+    
     return (
         <>
             <label>
